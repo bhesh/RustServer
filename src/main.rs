ceacdde74fd33f2762762106c15b761ce06fa05b
hessionb <hessionb@gmail.com>
2016-09-13 18:55:16 -0400

    Started testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::Write;
+use std::net::TcpListener;
+
+/// # RustServer
+///
+/// A simple server to demonstrate Rust functionality
+///
+/// Brian Hession - hessionb@gmail.com
 fn main() {
-    println!("Hello, world!");
+
+	// Todo: Allow arguments to define these
+	let host = "127.0.0.1";
+	let port = 8080;
+	println!("Listening on {}:{}", host, port);
+
+	// Create a new listener
+	let listener = TcpListener::bind(&format!("{}:{}", host, port)[..]).unwrap();
+
+	// Accept a connection
+	match listener.accept() {
+		Ok((_, addr)) => { println!("Got connection from {:?}", addr); }
+		Err(e) => { write!(std::io::stderr(), "{}", e.to_string()).unwrap(); }
+	}
+
+	// Clean up the listener
+	drop(listener);
 }
---
+++ src/net/server.rs
@@ -0,0 +1,5 @@
+pub struct Server {
+	pub hosts: Vec<String>,
+	pub port: u16,
+	pub dir: String,
+}
